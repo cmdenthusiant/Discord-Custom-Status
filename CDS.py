c846f91ab9dc0f61ed4ae0c3c756ac94bb9483c4
cmdenthusiant <53386624+cmdenthusiant@users.noreply.github.com>
2021-12-28 22:59:20 +0800

    Update CDS.py

--- CDS.py
+++ CDS.py
@@ -7,6 +7,9 @@ Interval = profile["Interval"]
 Status = profile["Status"]
 apiIp = "discord.com"
 
+def jslen(string):
+    return int(len(string.encode(encoding='utf_16_le'))/2)
+
 while True:
     for StatNum in range(len(Status)):
         Stat = Status[str(StatNum+1)]
@@ -14,13 +17,13 @@ while True:
         s = ssl.create_default_context().wrap_socket(sock,server_hostname="discord.com")
         s.connect((apiIp,443))
         head = "PATCH /api/v9/users/@me/settings HTTP/1.1\r\nHost: discord.com:443\r\n"
-        body = '{"custom_status":{"text":"%s"}}'%(Stat)
+        body = '{"custom_status":{"text":"%s","emoji_name":"%s"}}'%(Stat["text"],Stat["emoji_name"])
         cookie = "Cookie: %s\r\n"%Cookie
-        contentType = "Content-Type: application/json\r\nContent-Length: %s\r\n"%str(len(body))
+        contentType = "Content-Type: application/json\r\nContent-Length: %s\r\n"%str(jslen(body)+2)
         auth = "Authorization: "+TOKEN+"\r\n"
         data = head+auth+cookie+contentType+"\r\n"+body
         s.send(str.encode(data))
-        result = 'Success' if s.recv(1024).decode().split(" ")[1] == "200" else "Failed"
+        result = "Success" if s.recv(1024).decode().split(" ")[1] == "200" else "Failed"
         s.close()
-        print("Custom Status [%s]: %s"%(result,Stat))
+        print("Custom Status [%s]: %s%s"%(result,Stat["emoji_name"],Stat["text"]))
         time.sleep(int(Interval))
