765b7fee97fe90c39d9d8589b0f699e0bb9537dc
cmdenthusiant <53386624+cmdenthusiant@users.noreply.github.com>
2021-12-30 16:56:07 +0800

    Update CDS.py

--- CDS.py
+++ CDS.py
@@ -2,7 +2,6 @@ import socket,sys,json,time,ssl
 
 profile = json.loads(open("profile.json","rb").read())
 TOKEN = profile["TOKEN"]
-Cookie = profile["Cookie"]
 Interval = profile["Interval"]
 Status = profile["Status"]
 apiIp = "discord.com"
@@ -18,12 +17,11 @@ while True:
         s.connect((apiIp,443))
         head = "PATCH /api/v9/users/@me/settings HTTP/1.1\r\nHost: discord.com:443\r\n"
         body = '{"custom_status":{"text":"%s","emoji_name":"%s"}}'%(Stat["text"],Stat["emoji_name"])
-        cookie = "Cookie: %s\r\n"%Cookie
         contentType = "Content-Type: application/json\r\nContent-Length: %s\r\n"%str(jslen(body)+2)
         auth = "Authorization: "+TOKEN+"\r\n"
-        data = head+auth+cookie+contentType+"\r\n"+body
+        data = head+auth+contentType+"\r\n"+body
         s.send(str.encode(data))
-        result = "Success" if s.recv(1024).decode().split(" ")[1] == "200" else "Failed"
+        result = "Success" if s.recv(1024).decode().split(" ")[1] == "200" else s.recv(1024).decode()
         s.close()
         print("Custom Status [%s]: %s%s"%(result,Stat["emoji_name"],Stat["text"]))
         time.sleep(int(Interval))
